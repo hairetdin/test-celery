11311f43981e2e68dc53805c09090dd6574dc92e
hairetdin <amir.hairetdinov@gmail.com>
2017-11-18 21:26:13 +0500

    screenshots added

--- testtask/run_celery.sh
+++ testtask/run_celery.sh
@@ -3,3 +3,5 @@
 celery worker -E -l INFO -A testtask -Q import_value --hostname=localhost
 celery worker -E -l INFO -A testtask -Q calc_value --hostname=localhost
 celery worker -E -l INFO -A testtask -Q save_value --hostname=localhost
+
+celery flower-broker=amqp://localhost
